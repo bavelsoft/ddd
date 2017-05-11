bb5147e8824c76df7b48082e03e343747fb68a54
Joshua Spoerri <josh@Joshuas-MacBook-Pro.local>
2017-05-11 00:11:50 -0400

    delete dead files

--- src/main/java/com/bavelsoft/ddd/Validation.java
+++
@@ -1,8 +0,0 @@
-package com.bavelsoft.ddd;
-
-import java.util.function.Function;
-import java.util.function.Predicate;
-
-public interface Validation<T,R> extends Predicate<T>, Function<T,R> {
-}
-
