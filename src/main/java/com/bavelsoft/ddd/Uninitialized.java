8ef1e43594a0b5c0c0a43ed6a41c22e76b82efe2
Joshua Spoerri <josh@Joshuas-MacBook-Pro.local>
2017-07-05 23:32:29 -0400

    optimize imports

--- src/main/java/com/bavelsoft/ddd/Uninitialized.java
+++ src/main/java/com/bavelsoft/ddd/Uninitialized.java
@@ -1,10 +1,6 @@
 package com.bavelsoft.ddd;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.HashSet;
-import java.util.ArrayList;
-import java.util.function.Consumer;
 
 public interface Uninitialized<T,R> {
 	public T validate(Collection<R> validationResults);
